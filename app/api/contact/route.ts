bf30189490216ae695fbb00ff8f913601911113c
rehanmirza76 <abdulrehan0317@gmail.com>
2026-04-02 10:14:35 +0500

    added edge

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 const RECIPIENT_EMAIL = 'abdulrehan0317@gmail.com';
 
-export const runtime = 'nodejs';
+export const runtime = 'edge';
 
 export async function POST(request: NextRequest) {
   try {
